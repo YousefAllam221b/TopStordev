b5c8b9e5e3e8572315bc1c804a71d98a37866196
sdlfkj <dkdjkl@dljkf.com>
2023-09-06 22:57:54 +0300

    fixing

--- generateUpgradedDirs.sh
+++ generateUpgradedDirs.sh
@@ -6,15 +6,9 @@ then
 	openssl rand -out "aes256Pass.txt" 32
 elif [[ $1 = "encrypt" ]]
 then
+	zip -r zippedFolders /TopStor /pace /topstorweb
 	openssl aes-256-cbc -pass file:./aes256Pass.txt -in zippedFolders.zip -out encZippedFolders.zip
 	openssl pkeyutl -encrypt -pubin -inkey publicKey.pem -in aes256Pass.txt -out aes256PassCipher.txt
-elif [[ $1 = "decrypt" ]]
-then
-	openssl pkeyutl -decrypt -inkey privateKey.pem -in aes256PassCipher.txt -out aes256PassDecipher.txt
-	openssl aes-256-cbc -d -pass file:./aes256PassDecipher.txt -in encZippedFolders.zip -out decZippedFolders.zip
-elif [[ $1 = "zip" ]]
-then
-	zip -r zippedFolders /TopStor /pace /topstorweb
 else
 	echo "Please enter the argument!"
 fi
